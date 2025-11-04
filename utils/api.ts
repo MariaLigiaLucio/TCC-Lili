4ef87013f9d95e933f53a646743976c72d024209
Rodrigo Junior <165495511+rodrigojr09@users.noreply.github.com>
2025-11-04 01:29:20 -0300

    Update api.ts

--- utils/api.ts
+++ utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://192.168.0.16:3000/api",
+    baseURL: "https://tcc-classea.vercel.app/api",
 });
 
 export default api;
